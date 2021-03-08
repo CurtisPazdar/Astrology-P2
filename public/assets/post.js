0be2721171a3203c059b715e7cc610816ead2360
Kristen Campbell <kristenc1031@gmail.com>
2021-03-08 15:09:27 -0700

    changed background img

---
+++ public/assets/post.js
@@ -0,0 +1,36 @@
+const app = require("express");
+var db = require("../models");
+
+
+$(document).ready(function(){
+
+    var postBodyInput = $("#post-body");
+    
+
+    $("#post-submit").on("click", function() {
+        
+
+        console.log("button was clicked")
+
+        if (!postBodyInput.val().trim()) {
+            return;
+        }
+
+        var newPost = {
+            body: postBodyInput.val().trim(),
+            userId: userId
+        }
+
+        console.log(newPost);
+
+        submitPost(newPost);
+
+    });
+
+    function submitPost(Post) {
+        app.post("/api/posts/",Post, function() {
+            location.reload();
+        });
+
+    }
+})
