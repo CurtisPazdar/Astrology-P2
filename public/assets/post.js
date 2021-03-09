b8a25bfb2f9e806de506939cc9e5802f4f2182a6
Kristen Campbell <kristenc1031@gmail.com>
2021-03-08 17:34:52 -0700

    design stuff

---
+++ public/assets/post.js
@@ -0,0 +1,43 @@
+const app = require("express");
+var db = require("../models");
+
+
+$(document).ready(function () {
+  var postBodyInput = $("#post-body");
+
+  $("#post-submit").on("click", function () {
+    console.log("button was clicked");
+
+    var postBodyInput = $("#post-body");
+    
+
+
+    $("#post-submit").on("click", function (e) {
+        
+      e.preventDefault();
+
+        
+
+      console.log("button was clicked")
+
+      if (!postBodyInput.val().trim()) {
+        return;
+      }
+
+      var newPost = {
+        body: postBodyInput.val().trim(),
+        userId: userId,
+      };
+
+      console.log(newPost);
+
+      submitPost(newPost);
+    });
+
+    function submitPost(Post) {
+      app.post("/api/posts/", Post, function () {
+        location.reload();
+      });
+    }
+  });
+});
---
+++ routes/post-routes.js
@@ -0,0 +1,45 @@
+var db = require("../models");
+const app = require("express");
+
+module.exports = function(app) {
+
+
+    app.get("/message", function(req, res) {
+
+        console.log("/api/posts call made");
+        var query = {};
+        if (req.query.user_id) {
+          query.UserId = req.query.user_id;
+        }
+        db.Post.findAll({
+            where: query,
+            order: [
+				['createdAt', 'DESC'],
+				[db.Post, 'createdAt', 'ASC']	
+            ],
+            include: [
+                db.User, 
+                {
+                    model: db.Post,
+                    include: [ db.User],
+                }
+            ]
+            }).then(function(dbPost) {
+            res.json(dbPost);
+        });
+    });
+
+    app.post("/message/create", function(req, res) {
+        const id = req.params.id
+        console.log(req.body);
+        
+        db.Post.create({
+            body: req.body.body,
+            id: id,
+         
+        }).then(function(dbPost) {
+            res.json(dbPost);
+        });
+    });
+
+}
